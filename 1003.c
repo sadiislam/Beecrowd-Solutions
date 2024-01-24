657681745561dce661df155ecaf6c55aa132a3e6
Md. Sadiqul Islam <sadiqul.zeehad1@gmail.com>
2024-01-24 21:30:18 +0600

    Create 1003.c

---
+++ 1003.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,SOMA;
+    scanf("%d %d",&a,&b); //taking input
+    SOMA = a + b; //adding
+    printf("SOMA = %d\n",SOMA); //showing result(sum)
+    return 0;
+}
