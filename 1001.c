4d2919b3105a69eeceb69d520d6bdaffb56c8862
Md. Sadiqul Islam <sadiqul.zeehad1@gmail.com>
2024-01-24 21:20:16 +0600

    Create 1001.c

---
+++ 1001.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,X;
+    scanf("%d %d", &A, &B); //taking input
+    X = A + B; //adding
+    printf("X = %d\n", X); //showing result with endline
+    return 0;
+}
