6c33095b44db080b5ae0819b0d20018ef3c50acc
Md. Sadiqul Islam <sadiqul.zeehad1@gmail.com>
2024-01-24 21:27:27 +0600

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    double R,A,n;
+    n = 3.14159; //storing pi value in n
+    scanf("%lf",&R);
+    A=n*R*R; //calculating
+    printf("A=%.4lf\n",A);
+    return 0;
+}
+
