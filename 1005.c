e4545f0e5d1af9062bff3b35ec41812457caf6c9
Md. Sadiqul Islam <sadiqul.zeehad1@gmail.com>
2024-02-03 15:54:07 +0600

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,sum;
+    scanf("%f %f",&a,&b); //taking float
+    sum=(a*3.5 + b*7.5) / (3.5 + 7.5); //avarage calculating
+    printf("MEDIA = %.5f\n",sum);
+    return 0;
+}
+
