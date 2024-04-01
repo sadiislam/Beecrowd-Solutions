59fed0b1f29b0a27b08987c2278cd025b0a2684e
Md. Sadiqul Islam <sadiqul.zeehad1@gmail.com>
2024-04-01 14:48:35 +0600

    Create 1133.c

---
+++ 1133.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int x, y, z, i;
+    scanf("%d %d", &x, &y);
+    if(x > y){
+        z = x;
+        x = y;
+        y = z;
+    }
+    for(i=x+1; i<y; i++){
+        if(i%5 == 2 || i%5 == 3){
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
+
+
