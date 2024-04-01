1fec94cf0ab04e35a40c76ff08cb18704d16b980
Md. Sadiqul Islam <sadiqul.zeehad1@gmail.com>
2024-04-01 14:43:14 +0600

    Create 1132.c

---
+++ 1132.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int x, y, z, i, sum=0;
+    scanf("%d %d", &x, &y);
+    if(x > y){
+        z = x;
+        x = y;
+        y = z;
+    }
+    for(i=x; i<=y; i++){
+        if(i%13 != 0){
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+    return 0;
+}
+
